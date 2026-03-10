1ccccaa3a9224a252c0f074f7339c5429a18eaeb
Akshhaya Lakshmi Sankaranarayanan <alakshmi@DESKTOP-SASBTLTA.local>
2026-03-11 05:08:00 +0800

    commiting with new source code file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  rules: {
-    'no-console': 'warn',
-    'react-native/no-inline-styles': 'off',
-    'prettier/prettier': 'off',
-  },
 };
